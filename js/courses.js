912724813dc5a617205f5dd22fb1c9b678c8b27c
jeferbc <jeffe.bernal@gmail.com>
2017-02-16 22:22:27 -0500

    angular to show courses

---
+++ js/courses.js
@@ -0,0 +1,95 @@
+var courses = [
+  {
+    img_url: "img/image_course.png",
+    title: "PET",
+    area: "Biología",
+    level: "°6, 7°, 8°",
+    stars: "30",
+    faces: "3500"
+  },
+  {
+    img_url: "img/image_course.png",
+    title: "PET",
+    area: "Biología",
+    level: "°6, 7°, 8°",
+    stars: "30",
+    faces: "3500"
+  },
+  {
+    img_url: "img/image_course.png",
+    title: "PET",
+    area: "Biología",
+    level: "°6, 7°, 8°",
+    stars: "30",
+    faces: "3500"
+  },
+  {
+    img_url: "img/image_course.png",
+    title: "PET",
+    area: "Biología",
+    level: "°6, 7°, 8°",
+    stars: "30",
+    faces: "3500",
+  },
+  {
+    img_url: "img/image_course.png",
+    title: "PET",
+    area: "Biología",
+    level: "°6, 7°, 8°",
+    stars: "30",
+    faces: "3500"
+  },
+  {
+    img_url: "img/image_course.png",
+    title: "PET",
+    area: "Biología",
+    level: "°6, 7°, 8°",
+    stars: "30",
+    faces: "3500"
+  },
+  {
+    img_url: "img/image_course.png",
+    title: "PET",
+    area: "Biología",
+    level: "°6, 7°, 8°",
+    stars: "30",
+    faces: "3500"
+  },
+  {
+    img_url: "img/image_course.png",
+    title: "PET",
+    area: "Biología",
+    level: "°6, 7°, 8°",
+    stars: "30",
+    faces: "3500"
+  },
+  {
+    img_url: "img/image_course.png",
+    title: "PET",
+    area: "Biología",
+    level: "°6, 7°, 8°",
+    stars: "30",
+    faces: "3500",
+  },
+  {
+    img_url: "img/image_course.png",
+    title: "PET",
+    area: "Biología",
+    level: "°6, 7°, 8°",
+    stars: "30",
+    faces: "3500"
+  },
+  {
+    img_url: "img/image_course.png",
+    title: "PET",
+    area: "Biología",
+    level: "°6, 7°, 8°",
+    stars: "30",
+    faces: "3500"
+  }
+]
+
+var app = angular.module("app", []);
+app.controller("showCourses" ,function($scope){
+  $scope.courses = courses;
+});
